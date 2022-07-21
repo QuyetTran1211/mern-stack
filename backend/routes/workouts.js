19da2a0212b74d1d777b704257b99e2c28d6f1cc
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-07-21 17:32:01 +0700

    first commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET all workouts
+router.get('/', (req, res) => {
+  res.json({ message: 'GET all workouts' });
+});
+
+// GET single workout
+router.get('/:id', (req, res) => {
+  res.json({ message: 'GET single workout' });
+});
+
+// POST new workout
+router.post('/', (req, res) => {
+  res.json({ message: 'POST a new workout' });
+});
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'DELETE a new workout' });
+});
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+  res.json({ message: 'UPDATE a new workout' });
+});
+
+module.exports = router;
